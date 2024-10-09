15c81cef8f35204106232310dc10c853d4dadd09
TusharMishra1811 <tusharmishra1811@gmail.com>
2024-10-09 12:19:21 +0530

    Minor Changes

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -34,7 +34,7 @@ const MobileNav = () => {
               alt="Dhoom Logo"
               className="max-sm:size-10"
             />
-            <p className="text-[26px] font-extrabold text-white">Dhoom</p>
+            <p className="text-[26px] font-extrabold text-white">Yoom</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
